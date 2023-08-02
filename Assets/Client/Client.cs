ba7620a9a078ab2c3912a605b05af708408048fc
TheRad1 <18326@verona.k12.wi.us>
2023-08-02 00:22:21 -0500

    having issues actually getting the message

--- Assets/Client/Client.cs
+++ Assets/Client/Client.cs
@@ -4,18 +4,20 @@ using System.Net.Sockets;
 using System.Text;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Client: MonoBehaviour
 {
 	public int TCP_PORT = 4242;
 	public int UDP_PORT = 6969;
 	public string SERVER_IP = "127.0.0.1";
+	public int udpTimoutMS = 1000;
 
 	private void Start()
 	{
 		Debug.Log("starting server...");
 		// UDP client
-		using (UdpClient udpClient = new UdpClient())
+		/*using (UdpClient udpClient = new UdpClient())
 		{
 			//create message
 			string udpMessage = "Hello UDP Server!";
@@ -30,7 +32,9 @@ public class Client: MonoBehaviour
 
 			string receivedString = Encoding.ASCII.GetString(receiveBytes);
 			print("UDP Server says: " + receivedString);
-	}
+		}*/
+		udpReciever();
+		sendUDPMessage("Hi UDP (:");
 
 		// TCP client
 		using (TcpClient tcpClient = new TcpClient())
@@ -38,15 +42,58 @@ public class Client: MonoBehaviour
 			tcpClient.Connect(SERVER_IP, TCP_PORT);
 
 			NetworkStream tcpStream = tcpClient.GetStream();
-			string tcpMessage = "Hello TCP Server!";
+			string tcpMessage = "newClient~Test Username";
 			byte[] tcpData = Encoding.ASCII.GetBytes(tcpMessage);
+			byte[] tcpReceivedData = new byte[1024];
+
+			float startTime = Time.time;
 
 			tcpStream.Write(tcpData, 0, tcpData.Length);
-
-			byte[] tcpReceivedData = new byte[1024];
 			int bytesRead = tcpStream.Read(tcpReceivedData, 0, tcpReceivedData.Length);
+
+			float latency = Time.time - startTime;
+
+			Debug.Log("Latency of " + latency);
 			string tcpResponse = Encoding.ASCII.GetString(tcpReceivedData, 0, bytesRead);
 			Debug.Log("TCP Server says: " + tcpResponse);
 		}
+	}
+
+	async void udpReciever()
+	{
+		UdpClient udpClient = new UdpClient();
+		udpClient.Connect(SERVER_IP, UDP_PORT);
+		IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, UDP_PORT);
+
+		while(true)
+		{
+			byte[] receiveBytes = new byte[0];
+			await Task.Run(() => receiveBytes = udpClient.Receive(ref remoteEndPoint));
+			string receivedString = Encoding.ASCII.GetString(receiveBytes);
+			processUDPMessage(receivedString);
+		}
+	}
+
+	void sendUDPMessage(string message)
+	{
+		//load message
+		byte[] udpData = Encoding.ASCII.GetBytes(message);
+
+		//create client
+		UdpClient udpClient = new UdpClient(); 
+		udpClient.Connect(SERVER_IP, UDP_PORT);
+
+		//send message
+		udpClient.Send(udpData, udpData.Length);
+	}
+	
+	void processUDPMessage(string message)
+	{
+		Debug.Log("Got UDP message from server:\n" + message);
+	}
+
+	void processTCPMessage(string message)
+	{
+		Debug.Log("Got TCP message from server:\n" + message);
 	}
 }
